4cb28127b66d651dc592839c60382c874d30946b
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2026-08-27 11:39:28 -0500

    avace

--- src/config/global.js
+++ src/config/global.js
@@ -170,7 +170,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CF11_621209_DU.pdf',
+        download: 'downloads/621209_CF11_CFA.zip',
       },
       {
         icono: 'fas fa-download',
